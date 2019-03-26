8c403d91606d17462df835d86e51ccb8b7be70aa
Pablo R. Dinella <pablo.dinella@gmail.com>
2019-03-26 11:13:18 -0300

    use shelljs

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
 #!/usr/bin/env node
 
-// thanks https://stackoverflow.com/a/20643568/2350685
-const { exec } = require('child_process');
-exec('./playcs16.sh', (err, stdout, stderr) => {
-  if (err) {
-    // node couldn't execute the command
-    return;
-  }
-
-  // the *entire* stdout and stderr (buffered)
-  console.log(`stdout: ${stdout}`);
-  console.log(`stderr: ${stderr}`);
-});
+const shell = require('shelljs');
+shell.exec('./playcs16.sh');
