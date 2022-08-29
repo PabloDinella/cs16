72e146b667b08721b6df80d53714f9213d2f20bd
PabloDinella <pablo.dinella@gmail.com>
2022-08-29 12:00:05 -0300

    update url and deps

--- playcs16.sh
+++ playcs16.sh
@@ -4,9 +4,9 @@ if ! command -v wine; then
 	sudo apt install wine-stable -y
 fi
 
-zipped_file="$HOME/Downloads/Counter-Strike1.6Portable-SITECS.zip"
-zipped_file_url="http://www.comoeconomizar.com.br/Counter-Strike1.6Portable-SITECS.zip"
-file="$HOME/Downloads/Counter-Strike 1.6 Portable.exe"
+zipped_file="/tmp/Counter-Strike1.6Portable-SITECS.zip"
+zipped_file_url=$(curl --silent https://www.sitecs.net/counter-strike-1-6-portable | grep -o "http.*Counter-Strike1.6Portable-SITECS.zip")
+file="/tmp/Counter-Strike 1.6 Portable.exe"
 
 if [ -f "$file" ]; then
 	wine "$file"
@@ -14,13 +14,13 @@ if [ -f "$file" ]; then
 fi
 
 if [ ! -f "$zipped_file" ]; then
-	wget "$zipped_file_url" -P "$HOME/Downloads/"
+	wget "$zipped_file_url" -P "/tmp/"
 fi
 
 if ! command -v unzip; then
 	sudo apt install unzip -y
 fi
 
-unzip "$zipped_file" -d "$HOME/Downloads/"
+unzip "$zipped_file" -d "/tmp/"
 wine "$file"
 exit
